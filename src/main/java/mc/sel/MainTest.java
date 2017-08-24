b019190ce69e3f804ad81d0c85d057fe8083f4d9
Milan Crnjak <milan.crnjak@gmail.com>
2017-08-24 09:03:12 +0200

    Simple REPL for testing expressions

--- src/main/java/mc/sel/MainTest.java
+++ src/main/java/mc/sel/MainTest.java
@@ -14,53 +14,63 @@ import mc.sel.token.Token;
 import mc.sel.tokenizer.RegexTokenizer;
 import mc.sel.tokenizer.Tokenizer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 
 public class MainTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        String input = "pow(this.aaa.length() - 1, list(1,2).get(1)) * this.bbb - this.get('ccc').get(0) * 2";
-        String input = "this.aaa + ' ' + class('mc.sel.util.StringUtils').join('|', list('1','2','3')) + '|' " +
-                "+ class('java.lang.Math').pow(2.0, pow(2,3)).intValue()";
-        try {
-            // tokenize the input
-            Tokenizer tokenizer = new RegexTokenizer();
-            List<Token> tokens = tokenizer.tokenize(input);
+//        String input = "this.aaa + ' ' + class('mc.sel.util.StringUtils').join('|', list('1','2','3')) + '|' " +
+//                "+ class('java.lang.Math').pow(2.0, pow(2,3)).intValue()";
 
-            // parse the tokens
-            Parser parser = new DefaultParser();
-            ParseTreeNode parseTree = parser.parse(tokens);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String input = null;
+            while (!(input = br.readLine()).equalsIgnoreCase("q")) {
 
-            // parse tree to string
-            System.out.println(parseTree);
+                try {
+                    // tokenize the input
+                    Tokenizer tokenizer = new RegexTokenizer();
+                    List<Token> tokens = tokenizer.tokenize(input);
 
-            // stringify as lisp structure
-            ParseTreeVisitor<String> stringifier = new ParseTreeLispStringifier();
-            System.out.println(stringifier.visit(parseTree));
+                    // parse the tokens
+                    Parser parser = new DefaultParser();
+                    ParseTreeNode parseTree = parser.parse(tokens);
 
-            // context object
-            Map<String, Object> obj = new HashMap<>();
-            obj.put("aaa", "Hello");
-            obj.put("bbb", 10);
-            List<Integer> list = new ArrayList<>();
-            list.add(5);
-            list.add(15);
-            obj.put("ccc", list);
+                    // parse tree to string
+//                    System.out.println(parseTree);
 
-            // interpret
-            ParseTreeVisitor<Object> interpreter = new ParseTreeInterpreter(new MapContextObject(obj));
-            System.out.println(interpreter.visit(parseTree));
+                    // stringify as lisp structure
+                    ParseTreeVisitor<String> stringifier = new ParseTreeLispStringifier();
+                    System.out.println(stringifier.visit(parseTree));
 
-        } catch (TokenizerException e) {
-            System.err.println("Error in input at position [" + e.getPosition() + "] " + e.getMessage());
-        } catch (ParserException e) {
-            System.err.println("Error in input at token [" +
-                    e.getToken().getStartPos() + "," + e.getToken().getEndPos() + "] " + e.getMessage());
-        } catch (ParseTreeVisitorException e) {
-            System.err.println("Error in parse tree visitor.");
-            e.printStackTrace();
+                    // context object
+                    Map<String, Object> obj = new HashMap<>();
+                    obj.put("aaa", "Hello");
+                    obj.put("bbb", 10);
+                    List<Integer> list = new ArrayList<>();
+                    list.add(5);
+                    list.add(15);
+                    obj.put("ccc", list);
+
+                    // interpret
+                    ParseTreeVisitor<Object> interpreter = new ParseTreeInterpreter(new MapContextObject(obj));
+                    System.out.println(interpreter.visit(parseTree));
+
+                } catch (TokenizerException e) {
+                    System.err.println("Error in input at position [" + e.getPosition() + "] " + e.getMessage());
+                } catch (ParserException e) {
+                    System.err.println("Error in input at token [" +
+                            e.getToken().getStartPos() + "," + e.getToken().getEndPos() + "] " + e.getMessage());
+                } catch (ParseTreeVisitorException e) {
+                    System.err.println("Error in parse tree visitor.");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
